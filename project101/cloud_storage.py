54159be45f4cc5b960b275c2f6f5afa51b56c8d7
SamLegodino-7124 <83705032+SamLegodino-7124@users.noreply.github.com>
2021-06-01 15:34:24 +0530

    Update cloud_storage.py

--- project101/cloud_storage.py
+++ project101/cloud_storage.py
@@ -12,7 +12,7 @@ class TransferData:
         dbx.files_upload(f.read(), file_to)
 
 def main():
-    access_token = 'sl.AbKQY7cwlr949HZB7JxLOMrnYKuY39PSkiEnMjmzkLJ8mukldzSQjT8oLVfn_A-kB4yn6O0erRD07aV-9JeaGvvRPoLFEVvwg3_p2AufnKjhGlCgTVtpR4YV0SKhk6nbU2-ztZAB'
+    access_token = 'r4c46ggxEoEAAAAAAAAAATxdy1dx47RViXTZ4xCTvbJBsnf86FKH0zXR2m9bU50N'
     transferData = TransferData(access_token)
 
     file_from = input("Enter the file path to transfer : -")
